3633f02f2e2bbb3e90b92a54385321fcd5995a54
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 17:06:31 +0800

    Add button to switch panels

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import { Button, Col, Container, Row } from "reactstrap";
+
+enum Panel {
+  Product = "Product",
+  Order = "Order",
+}
+
 export default function App() {
-  return <div>Hello World!</div>;
+  const [panel, setPanel] = useState(Panel.Product);
+  return (
+    <Container className="mt-3" fluid>
+      <Row style={{ justifyContent: "center" }}>
+        <Col xl="auto" lg="auto" md="auto" sm="auto" xs="auto">
+          <Button
+            className="mx-3"
+            color={panel === Panel.Product ? "primary" : undefined}
+            onClick={() => setPanel(Panel.Product)}
+          >
+            Product Listing
+          </Button>
+
+          <Button
+            className="mx-3"
+            color={panel === Panel.Order ? "primary" : undefined}
+            onClick={() => setPanel(Panel.Order)}
+          >
+            Order Listing
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
