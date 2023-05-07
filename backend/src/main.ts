0aed1f61ff62cc64fad1a40686bbb0a7c132966c
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 17:06:14 +0800

    Allow cors for localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['localhost:3000'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
