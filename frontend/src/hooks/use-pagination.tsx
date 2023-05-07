ef6750d3e595f0f5c6b924b68332b7abca9c5fa4
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 20:59:47 +0800

    Remove get first data on use pagination

--- frontend/src/hooks/use-pagination.tsx
+++ frontend/src/hooks/use-pagination.tsx
@@ -1,73 +1,51 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { PaginationOption } from "../interfaces/pagination-options.interface";
 import { PaginationResults } from "../interfaces/pagination-results.interface";
 
 export function usePagination<T>(
   size: number,
   fetcher: (
-    payload: any,
     paginationOption?: PaginationOption,
     pageUrl?: string
   ) => Promise<PaginationResults<T>>
 ) {
-  const [payload, setPayload] = useState<any>();
-  const [currentPage, setCurrentPage] = useState<number | null>(null);
-  const [currentData, setCurrentData] = useState<PaginationResults<T>>();
-
+  const [nextPageUrl, setNextPageUrl] = useState<string | null>(null);
+  const [previousPageUrl, setPreviousPageUrl] = useState<string | null>(null);
   const goToPage = useCallback(
-    async (page: number): Promise<void> => {
+    async (page: number): Promise<PaginationResults<T> | null> => {
       const paginationOption: PaginationOption = {
         page,
         size,
       };
 
       try {
-        const result = await fetcher(payload, paginationOption);
-        setCurrentPage(result.page);
-        setCurrentData(result);
+        const result = await fetcher(paginationOption);
+        setNextPageUrl(result.nextPageUrl);
+        setPreviousPageUrl(result.previousPageUrl);
+        return result;
       } catch {
-        setCurrentPage(null);
+        return null;
       }
     },
-    [fetcher, payload, size]
+    [fetcher, size]
   );
 
-  const goToUrl = async (url: string): Promise<void> => {
-    try {
-      const result = await fetcher(payload, void 0, url);
-      setCurrentPage(result.page);
-      setCurrentData(result);
-    } catch {
-      setCurrentPage(null);
-    }
-  };
+  const goToUrl = useCallback(
+    async (url: string): Promise<PaginationResults<T> | null> => {
+      try {
+        const result = await fetcher(void 0, url);
+        setNextPageUrl(result.nextPageUrl);
+        setPreviousPageUrl(result.previousPageUrl);
+        return result;
+      } catch {
+        return null;
+      }
+    },
+    [fetcher]
+  );
 
-  const ready = useMemo(() => {
-    const ready = currentPage !== null;
-    if (!ready) {
-      goToPage(1);
-    }
-    return ready;
-  }, [currentPage, goToPage]);
+  const nextPage = () => nextPageUrl && goToUrl(nextPageUrl);
+  const previousPage = () => previousPageUrl && goToUrl(previousPageUrl);
 
-  const hasNextPage = useMemo(() => {
-    return !!currentData?.nextPageUrl;
-  }, [currentData]);
-
-  const hasPreviousPage = useMemo(() => {
-    return !!currentData?.previousPageUrl;
-  }, [currentData]);
-
-  const nextPage = () => hasNextPage && goToUrl(currentData!.nextPageUrl);
-  const previousPage = () =>
-    hasPreviousPage && goToUrl(currentData!.previousPageUrl);
-
-  return [
-    ready,
-    currentData,
-    setPayload,
-    nextPage,
-    previousPage,
-    goToPage,
-  ] as const;
+  return [nextPage, previousPage, goToPage] as const;
 }
