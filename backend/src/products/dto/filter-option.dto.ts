fe6ce1135f06e9edc5d1d25ba59dd3e14f3ec283
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 20:59:27 +0800

    Fix filter option array of number

--- backend/src/products/dto/filter-option.dto.ts
+++ backend/src/products/dto/filter-option.dto.ts
@@ -1,18 +1,15 @@
-import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class FilterOptionDto {
-  @IsNumber({ maxDecimalPlaces: 0 })
-  @IsArray()
+  @IsNumber({ maxDecimalPlaces: 0 }, { each: true })
   @IsOptional()
   categoryIds: number[] | null;
 
-  @IsNumber({ maxDecimalPlaces: 0 })
-  @IsArray()
+  @IsNumber({ maxDecimalPlaces: 0 }, { each: true })
   @IsOptional()
   brandIds: number[] | null;
 
-  @IsNumber({ maxDecimalPlaces: 0 })
-  @IsArray()
+  @IsNumber({ maxDecimalPlaces: 0 }, { each: true })
   @IsOptional()
   colorIds: number[] | null;
 
