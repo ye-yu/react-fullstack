f46efb5f726fbb3b877da2909b740ae3d8505e24
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 11:04:55 +0800

    Allow filter products by name, brands, and colors

--- backend/src/products/dto/filter-option.dto.ts
+++ backend/src/products/dto/filter-option.dto.ts
@@ -1,8 +1,22 @@
-import { IsArray, IsNumber, IsOptional } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class FilterOptionDto {
   @IsNumber({ maxDecimalPlaces: 0 })
   @IsArray()
   @IsOptional()
   categoryIds: number[] | null;
+
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @IsArray()
+  @IsOptional()
+  brandIds: number[] | null;
+
+  @IsNumber({ maxDecimalPlaces: 0 })
+  @IsArray()
+  @IsOptional()
+  colorIds: number[] | null;
+
+  @IsString()
+  @IsOptional()
+  name: string;
 }
--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ProductRepo } from '../database/repos/product.repo';
 import { FilterOptionDto } from './dto/filter-option.dto';
 import { ProductEntity } from '../database/entities/product.entity';
-import { FindOptionsWhere, In } from 'typeorm';
+import { FindOptionsWhere, ILike, In } from 'typeorm';
 import { PaginationOptionDto } from './dto/pagination-options.dao';
 import { PaginationResults } from './dao/pagination-results.dao';
 
@@ -18,6 +18,19 @@ export class ProductsService {
     if (filter.categoryIds) {
       where.categoryId = In(filter.categoryIds);
     }
+
+    if (filter.brandIds) {
+      where.brandId = In(filter.brandIds);
+    }
+
+    if (filter.colorIds) {
+      where.colorId = In(filter.colorIds);
+    }
+
+    if (filter.name) {
+      where.name = ILike(`%${filter.name}%`);
+    }
+
     const total = await this.productsRepo.count({
       where,
     });
