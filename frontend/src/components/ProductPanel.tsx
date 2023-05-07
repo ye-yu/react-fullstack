46f3f7e41cdbecba52a0d14d7aff10e5cd1055a6
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 18:38:42 +0800

    Render search inputs and capture values

--- frontend/src/components/ProductPanel.tsx
+++ frontend/src/components/ProductPanel.tsx
@@ -8,6 +8,10 @@ import {
   Label,
   Input,
   Button,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
 } from "reactstrap";
 import { getSearchOptions } from "../http/get-search-option";
 import { SearchOptions } from "../interfaces/search-options.interface";
@@ -17,15 +21,46 @@ export function ProductPanel() {
     null
   );
 
+  const [searchInputs, setSearchInputs] = useState<Record<string, any>>({});
+
+  const [dropdownOpenState, setDropdownOpenState] = useState<
+    Record<string, boolean>
+  >({});
+
   const searchAvailable = useMemo(() => {
     const searchAvailable = !!searchOptions;
     if (!searchAvailable) {
-      getSearchOptions().then((newSearchOption) => {
-        setSearchOptions(newSearchOption);
+      getSearchOptions().then((newSearchOptions) => {
+        setSearchOptions(newSearchOptions);
+        for (const option of newSearchOptions) {
+          setSearchInputs((previousValues) => ({
+            ...previousValues,
+            [option.name]: option.type === "text" ? "" : [],
+          }));
+          if (option.type === "dropdown") {
+            setDropdownOpenState((previous) => ({
+              ...previous,
+              [option.name]: false,
+            }));
+          }
+        }
       });
     }
     return searchAvailable;
   }, [searchOptions]);
+
+  const dropdownIdToLabel = useMemo(() => {
+    if (!searchOptions) return {} as Record<string, Record<number, string>>;
+    const dropdownOptions = searchOptions.filter((e) => e.type === "dropdown");
+    return dropdownOptions.reduce((a, b) => {
+      a[b.name] = {};
+      for (const { id, label } of b.options) {
+        a[b.name][id] = label;
+      }
+      return a;
+    }, {} as Record<string, Record<number, string>>);
+  }, [searchOptions]);
+
   return (
     <Row className="mt-3">
       {/* search panel */}
@@ -47,17 +82,99 @@ export function ProductPanel() {
               <Form>
                 {searchAvailable &&
                   searchOptions?.map((option) => {
-                    return (
-                      <FormGroup key={option.name}>
-                        <Label for={option.name}>{option.label}</Label>
-                        <Input
-                          id={option.name}
-                          name={option.name}
-                          placeholder={option.placeholder}
-                          type="text"
-                        />
-                      </FormGroup>
-                    );
+                    switch (option.type) {
+                      case "text":
+                        return (
+                          <FormGroup key={option.name}>
+                            <Label for={option.name}>{option.label}</Label>
+                            <Container className="px-1">
+                              <Row className="gx-0">
+                                <Col xs="12">
+                                  <Input
+                                    style={{ borderColor: "#6c757d" }}
+                                    id={option.name}
+                                    name={option.name}
+                                    placeholder={option.placeholder}
+                                    type="text"
+                                    onChange={(event) =>
+                                      setSearchInputs((previous) => ({
+                                        ...previous,
+                                        [option.name]: event.target.value || "",
+                                      }))
+                                    }
+                                  />
+                                </Col>
+                              </Row>
+                            </Container>
+                          </FormGroup>
+                        );
+                      case "dropdown":
+                        return (
+                          <FormGroup key={option.name}>
+                            <Label for={option.name}>{option.label}</Label>
+                            <Container className="px-1">
+                              <Row className="gx-0">
+                                <Col xs="12">
+                                  <Dropdown
+                                    isOpen={dropdownOpenState[option.name]}
+                                    toggle={() =>
+                                      setDropdownOpenState((previous) => ({
+                                        ...previous,
+                                        [option.name]: !previous[option.name],
+                                      }))
+                                    }
+                                  >
+                                    <DropdownToggle
+                                      className="w-100"
+                                      style={{ textAlign: "left" }}
+                                      caret
+                                      outline
+                                    >
+                                      <span
+                                        className="d-inline-block"
+                                        style={{
+                                          width: "95%",
+                                          color: searchInputs[
+                                            option.name
+                                          ]?.find((e: any) => e)
+                                            ? "black"
+                                            : "inherit",
+                                        }}
+                                      >
+                                        {dropdownIdToLabel[option.name]?.[
+                                          searchInputs[option.name]?.find(
+                                            (e: any) => e
+                                          )
+                                        ] ||
+                                          option.placeholder ||
+                                          "Select an Option"}
+                                      </span>
+                                    </DropdownToggle>
+                                    <DropdownMenu>
+                                      {option.options.map(({ id, label }) => (
+                                        <DropdownItem
+                                          onClick={() => {
+                                            setSearchInputs((previous) => ({
+                                              ...previous,
+                                              [option.name]: [id],
+                                            }));
+                                          }}
+                                          key={id}
+                                        >
+                                          {label}
+                                        </DropdownItem>
+                                      ))}
+                                    </DropdownMenu>
+                                  </Dropdown>
+                                </Col>
+                              </Row>
+                            </Container>
+                          </FormGroup>
+                        );
+
+                      default:
+                        return null;
+                    }
                   })}
                 <Button
                   className="w-100 mt-3"
