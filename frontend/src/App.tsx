bcbd50ca9708774b7d320cd83ddf718bb20c1c7f
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 22:10:38 +0800

    Use  button group on top nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Col, Container, Row } from "reactstrap";
+import { Button, ButtonGroup, Col, Container, Row } from "reactstrap";
 import { ProductPanel } from "./components/ProductPanel";
 
 enum Panel {
@@ -14,21 +14,23 @@ export default function App() {
     <Container className="mt-3" fluid>
       <Row style={{ justifyContent: "center" }}>
         <Col xs="auto">
-          <Button
-            className="mx-3"
-            color={panel === Panel.Product ? "primary" : undefined}
-            onClick={() => setPanel(Panel.Product)}
-          >
-            Product Listing
-          </Button>
+          <ButtonGroup>
+            <Button
+              outline={panel !== Panel.Product}
+              color={panel === Panel.Product ? "primary" : undefined}
+              onClick={() => setPanel(Panel.Product)}
+            >
+              Product Listing
+            </Button>
 
-          <Button
-            className="mx-3"
-            color={panel === Panel.Order ? "primary" : undefined}
-            onClick={() => setPanel(Panel.Order)}
-          >
-            Order Listing
-          </Button>
+            <Button
+              outline={panel !== Panel.Order}
+              color={panel === Panel.Order ? "primary" : undefined}
+              onClick={() => setPanel(Panel.Order)}
+            >
+              Order Listing
+            </Button>
+          </ButtonGroup>
         </Col>
       </Row>
       {panel === Panel.Product && <ProductPanel />}
