fca47c9da3055a955cbdf33cbb9cb0286d02f412
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 09:44:03 +0800

    Update database source entities and migration

--- backend/src/database/database.source.ts
+++ backend/src/database/database.source.ts
@@ -3,12 +3,20 @@ import { config } from 'dotenv';
 import { BrandEntity } from './entities/brand.entity';
 import { ProductEntity } from './entities/product.entity';
 import { CategoryEntity } from './entities/category.entity';
-import { ProductColorsEntity } from './entities/product-color.entity';
+import { ColorsEntity } from './entities/product-color.entity';
+import { OrderHistoryEntity } from './entities/order.entity';
+import { CreateTables1683420792527 } from './migrations/1683420792527-CreateTables';
 config();
 
 export const defaultDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: [BrandEntity, ProductEntity, CategoryEntity, ProductColorsEntity],
-  migrations: [],
+  entities: [
+    BrandEntity,
+    ProductEntity,
+    CategoryEntity,
+    ColorsEntity,
+    OrderHistoryEntity,
+  ],
+  migrations: [CreateTables1683420792527],
 });
