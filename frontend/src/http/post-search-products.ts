6f03f13cb6b22a495362e104e4f96587e10b0f4c
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-05-07 19:55:04 +0800

    Add post search products function

---
+++ frontend/src/http/post-search-products.ts
@@ -0,0 +1,45 @@
+import { PaginationOption } from "../interfaces/pagination-options.interface";
+import { PaginationResults } from "../interfaces/pagination-results.interface";
+import { ProductEntity } from "../interfaces/product.interface";
+
+export async function postSearchProducts(
+  searchInputs: Record<string, any>,
+  paginationOption?: PaginationOption,
+  /** pass here if using nextPageUrl or previousPageUrl */
+  pageUrl?: string
+): Promise<PaginationResults<ProductEntity>> {
+  const emptyPagination: PaginationResults<ProductEntity> = {
+    nextPageUrl: "",
+    previousPageUrl: "",
+    page: 0,
+    size: 0,
+    total: 0,
+    results: [],
+  };
+  if (!process.env.REACT_APP_BACKEND_URL) return emptyPagination;
+  try {
+    const url = pageUrl
+      ? new URL(pageUrl)
+      : new URL("/products/search-options", process.env.REACT_APP_BACKEND_URL);
+    if (paginationOption) {
+      url.searchParams.set("page", `${paginationOption.page}`);
+      url.searchParams.set("size", `${paginationOption.size}`);
+    }
+    const request = await fetch(url, {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(searchInputs),
+    });
+    if (request.headers.get("content-type")?.includes("application/json")) {
+      const parsedJson = await request.json();
+      return parsedJson.search;
+    } else {
+      return emptyPagination;
+    }
+  } catch (error) {
+    console.error(error);
+    return emptyPagination;
+  }
+}
